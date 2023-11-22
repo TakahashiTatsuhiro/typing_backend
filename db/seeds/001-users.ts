db318a22c2c5fa78b6420adddb65b26cd0256421
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-22 11:44:56 +0900

    deploy07

--- db/seeds/001-users.ts
+++ db/seeds/001-users.ts
@@ -2,6 +2,7 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
+    await knex("results").del();
     await knex('users').del()
     await knex('users').insert([
       {username: 'admin', password:"admin"},
